85e929652e1fa4bdf527ad89982a4a3593440c1e
Tom Dietrich <tom.dietrich@gmail.com>
2015-03-30 09:21:06 -0400

    Execute Program Command Implemented

---
+++ BoxLaunch/Actions/CleanDirectoryAction.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BoxLaunch.Actions
+{
+    public class CleanDirectoryAction : IAction
+    {
+        public string TargetPath { get; set; }
+        public DirectoryInfo TargetDir { get; set; }
+
+
+        public bool Execute()
+        {
+            if (string.IsNullOrWhiteSpace(TargetPath))
+            {
+                Console.WriteLine("ERROR: No target path was provided!");
+                return false;
+            }
+
+            if (!Directory.Exists(TargetPath))
+            {
+                Console.WriteLine("ERROR: Target directory ({0}) does not exist!", TargetPath);
+                return false;
+            }
+
+            TargetDir = new DirectoryInfo(TargetPath);
+            var files = TargetDir.GetFiles().ToList();
+
+            try
+            {
+                foreach (var file in files)
+                {
+                    file.Delete();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: Exception cleaning directory: " + ex.ToString());
+                return false;
+            }
+            
+
+            return true;
+        }
+    }
+}
---
+++ BoxLaunch/Commands/ExecuteProgramCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BoxLaunch.Actions;
+using NDesk.Options;
+
+namespace BoxLaunch.Commands
+{
+    public class ExecuteProgramCommand : BaseCommand
+    {
+        public string TargetPath { get; set; }
+
+        public string ExecutableName { get; set; }
+        public List<string> ExecutableArgs { get; set; }
+
+        public override void Run(IEnumerable<string> args)
+        {
+            ExecutableArgs = new List<string>();
+
+            var p = new OptionSet
+            {
+                { "t|target=", "The {TARGET DIRECTORY} containing the program to be executed", v => TargetPath = v.EndsWith("\\") ? v : v + "\\" },
+                { "p|program=", "The {PROGRAM} to execute.", v => ExecutableName = v },
+                { "a|arg=", "An {ARGUMENT} that should be passed to the executable.", ExecutableArgs.Add }
+            };
+
+            var extra = Parse(
+                p,
+                args,
+                "execute-program",
+                "-t={TARGET DIRECTORY} -p={PROGRAM}",
+                "Executes a program.");
+
+            if (extra == null) return;
+
+            var executeAction = new RunExecutableAction
+            {
+                TargetPath = TargetPath,
+                ExecutableName = ExecutableName,
+                ExecutableArgs = ExecutableArgs
+            };
+
+            executeAction.Execute();
+        }
+    }
+}
